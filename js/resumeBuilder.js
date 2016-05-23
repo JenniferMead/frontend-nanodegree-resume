0f6e9c12a0df88fd10729d08ede788d22f32021d
Jennifer Mead <jennifer.e.mead@gmail.com>
2016-05-23 12:19:42 -0700

    I commited my final changes before submitting the project.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,14 +5,94 @@ var bio = {
   "role": "Front-end web-developer",
     "contacts": {
     "mobile": "267-555-5555",
-    "location": "Napa, Ca",
     "email": "myemail@gmail.com",
-    "gitHubUserName": "JenniferMead"
+    "gitHubUserName": "JenniferMead",
+    "location": "Napa, Ca"
   },
+  "WelcomeMessage": "Welcome to my interactive resume! I'm an aspiring web developer with a passion for coding and wine.",
   "skills": ["HTML", "CSS", "Javascript", "Design"],
   "bioPic": ["images/profile.jpg"],
-  "WelcomeMessage": "Welcome to my interactive resume! I'm an aspiring web developer with a passion for coding and wine."
+  "display": function() {
+      var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.WelcomeMessage);
+      $("#header").prepend(formattedMsg);
+      var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+      $("#header").prepend(formattedRole);
+      var formattedName = HTMLheaderName.replace("%data%", bio.name);
+      $("#header").prepend(formattedName);
+      var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+      $('#header').prepend(formattedPic);
+      $("#topContacts").append(HTMLcontactStart);
+      var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+      $("#topContacts").append(formattedMobile);
+      $("#footerContacts").append(formattedMobile);
+      var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+      $("#topContacts").append(formattedEmail);
+      $("#footerContacts").append(formattedEmail);
+  	  var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.gitHubUserName);
+      $("#topContacts").append(formattedGitHub);
+      $("#footerContacts").append(formattedGitHub);
+  	  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+      $("#topContacts").append(formattedLocation);
+      $("#footerContacts").append(formattedLocation);
+      $("#header").append(HTMLskillsStart);
+      for(var skill in bio.skills) {
+  		var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+  		$("#skills").append(formattedSkill);
+      }
+  }
 };
+
+
+var education = {
+  "schools": [
+{
+  "name": "UC Davis",
+  "location": "Davis, Ca",
+  "degree": "Masters",
+  "major": ["Immunology"],
+  "dates": "2012-2014",
+  "url": "https://www.ucdavis.edu/"
+},
+{
+  "name": "Moravian",
+  "location": "Bethlehem, Pa",
+  "degree": "Bachelors",
+  "major": ["Biology"],
+  "dates": "2007-2011",
+  "url": "http://www.moravian.edu/"
+}
+],
+"onlineCourses":[
+{
+  "title": "Front-End Web-Developer Nanodegree",
+  "school": "Udacity",
+  "dates": "02/2016 - current",
+  "url": "https://www.udacity.com/"
+}
+],
+"display": function (){
+for(school in education.schools){
+  $("#education").append(HTMLschoolStart);
+  var formattedSchool = HTMLschoolName.replace("#",education.schools[school].url ).replace("%data%", education.schools[school].name);
+  var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+  var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+  var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+  var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+  $(".education-entry:last").append(formattedSchool + formattedLocation + formattedDegree + formattedDates + formattedMajor);
+}
+for(course in education.onlineCourses){
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+    var formattedOnlineTitle = HTMLonlineTitle.replace("#",education.onlineCourses[course].url).replace("%data%", education.onlineCourses[course].title);
+    var formattedOnlineSchoolTitle = formattedOnlineTitle + formattedOnlineSchool;
+    var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+    $("#education").append(HTMLonlineClasses);
+    $("#education").append(HTMLschoolStart);
+    $(".education-entry:last").append(formattedOnlineSchoolTitle);
+    $(".education-entry:last").append(formattedOnlineDates);
+}
+}
+};
+
 
 var work = {
   "jobs": [
@@ -20,7 +100,7 @@ var work = {
   "employer": "Alpha Omega",
   "title": "Tasting Room Associate",
   "location": "Napa, Ca",
-  "dates": "03/2016 - current",
+  "dates": "03/2016 - in progress",
   "description": "I sell wine in a tasting room"
 },
 {
@@ -30,15 +110,31 @@ var work = {
   "dates": "08/2016 - 12/2016",
   "description": "I worked in a wine lab helping to make sure the wine fermented properly"
 }
-]
+],
+"display": function (){
+for(job in work.jobs){
+  $("#workExperience").append(HTMLworkStart);
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+  $(".work-entry:last").append(formattedEmployerTitle);
+  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+  $(".work-entry:last").append(formattedDates);
+  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  $(".work-entry:last").append(formattedDescription);
+  var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+  $(".work-entry:last").append(formattedLocation);
+}
+}
 };
+
 
 var projects =
 {
 "projects": [
 {
   "title": "Wine Blog",
-  "dates": "05/2016 - current",
+  "dates": "05/2016 - in progress",
   "description": "A blog which reviews and rates cheap wines to help people determine whether they are worth buying",
   "images": ["images/wine.jpg"]
 },
@@ -48,87 +144,8 @@ var projects =
   "description": "A portfolio of my projects",
   "images": ["images/logo.jpg"]
 }
-]
-};
-
-var education = {
-  "schools": [
-{
-  "name": "UC Davis",
-  "location": "Davis, Ca",
-  "degree": "Masters",
-  "major": "Immunology",
-  "dates": "2012-2014",
-  "url": "https://www.ucdavis.edu/"
-},
-{
-  "name": "Moravian",
-  "location": "Bethlehem, Pa",
-  "degree": "Bachelors",
-  "major": "Biology",
-  "dates": "2007-2011",
-  "url": "http://www.moravian.edu/"
-}
 ],
-"onlineCourses":[
-{
-  "school": "Udacity",
-  "title": "Front-End Web-Developer Nanodegree",
-  "dates": "02/2016 - current",
-  "url": "https://www.udacity.com/"
-}
-]
-};
-
-
-
-//Code used to input my information into the appropriate locations on the resume
-
-
-//code that allows bio information to be shown
-
-
-bio.display = function() {
-
-    var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.WelcomeMessage);
-    $("#header").prepend(formattedMsg);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    $("#header").prepend(formattedRole);
-    var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    $("#header").prepend(formattedName);
-    var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-    $('#header').prepend(formattedPic);
-
-
-    $("#topContacts").append(HTMLcontactStart);
-    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-    $("#topContacts").append(formattedMobile);
-    $("#footerContacts").append(formattedMobile);
-    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-    $("#topContacts").append(formattedEmail);
-    $("#footerContacts").append(formattedEmail);
-	  var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.gitHubUserName);
-    $("#topContacts").append(formattedGitHub);
-    $("#footerContacts").append(formattedGitHub);
-	  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-    $("#topContacts").append(formattedLocation);
-    $("#footerContacts").append(formattedLocation);
-
-    $("#header").append(HTMLskillsStart);
-
-
-  for(var skill in bio.skills) {
-		var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-		$("#skills").append(formattedSkill);
-}
-};
-
-
-bio.display();
-
-
-//this adds a method to projects which allows them to be diplayed
-projects.display = function() {
+"display": function() {
 for(project /*index in the array called projects*/in projects/*object called projects*/.projects/*array also called projects*/){
 $("#projects").append(HTMLprojectStart);
 var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
@@ -137,7 +154,6 @@ var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[projec
 $(".project-entry:last").append(formattedDates);
 var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description)
 $(".project-entry:last").append(formattedDescription);
-
 if(projects.projects[project].images.length > 0) {
   for (image in projects.projects[project].images) {
     var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
@@ -146,60 +162,34 @@ if(projects.projects[project].images.length > 0) {
 }
 }
 }
+};
+
+
+
+//Code used to call the methods to display all of my information on the resume
+
+bio.display();
 projects.display();
-
-
-//this is a function, but noto a method within an object, that allows the work to be displayed
-work.display = function (){
-for(job in work.jobs){
-  $("#workExperience").append(HTMLworkStart);
-  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-  var formattedEmployerTitle = formattedEmployer + formattedTitle;
-  $(".work-entry:last").append(formattedEmployerTitle);
-
-  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-  $(".work-entry:last").append(formattedDates);
-
-  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-  $(".work-entry:last").append(formattedDescription);
-
-  var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-  $(".work-entry:last").append(formattedLocation);
-}
-}
-
 work.display();
-
-
-//nothing is showing education!
-
-
-education.display = function (){
-for(school in education.schools){
-  var formattedSchool = HTMLschoolName.replace("%data%", education.schools[school].name);
-  var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-  var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-  var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-  var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
-  $("#education").append(HTMLschoolStart);
-  $(".education-entry:last").append(formattedSchool + formattedLocation + formattedDegree + formattedDates + formattedMajor);
-}
-for(course in education.onlineCourses){
-    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
-    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
-    var formattedOnlineSchoolTitle = formattedOnlineSchool + formattedOnlineTitle;
-    var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
-    var formattedOnlineUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
-    $("#education").append(HTMLonlineClasses);
-    $("#education").append(HTMLschoolStart);
-    $(".education-entry:last").append(formattedOnlineSchoolTitle);
-      $(".education-entry:last").append(formattedOnlineDates + formattedOnlineUrl);
-}
-}
-
 education.display();
 
+
+//Code used to internationalize my name
+$("#main").append(internationalizeButton);
+
+var inName = function(name){
+  var internationalName = [];
+  internationalName = name.split(" ");
+  internationalName[0] = internationalName[0].slice(0,1).toUpperCase() + internationalName[0].slice(1).toLowerCase();
+  internationalName[1] = internationalName[1].toUpperCase();
+  return internationalName.join(" ");
+};
+
+inName(bio.name);
+
+
+//Code used to allow googleMaps to show my locations
+$("#mapDiv").append(googleMap);
 
 //Do I need this? It just shows were I clicked on the page.
 $(document).click(function(loc) {
@@ -220,21 +210,3 @@ for (job /*an index in the array*/ in work_obj/*the object*/.jobs/*an array with
 }
 
 console.log(locationizer(work));
-
-
-//Code used to internationalize my name
-$("#main").append(internationalizeButton);
-
-var inName = function(name){
-  var internationalName = [];
-  internationalName = name.split(" ");
-  internationalName[0] = internationalName[0].slice(0,1).toUpperCase() + internationalName[0].slice(1).toLowerCase();
-  internationalName[1] = internationalName[1].toUpperCase();
-  return internationalName.join(" ");
-};
-
-inName(bio.name);
-
-
-//Code used to allow googleMaps to show my locations
-$("#mapDiv").append(googleMap);
